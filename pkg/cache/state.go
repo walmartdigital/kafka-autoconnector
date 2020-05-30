2151e59f79a931a4ff3b28353b58be90d87ee784
chinniehendrix <chinniehendrix@gmail.com>
2020-05-30 11:32:09 -0400

    feat(cache): add cache sigletons for centrally sharing configuration and connector state

---
+++ pkg/cache/state.go
@@ -0,0 +1,32 @@
+package state
+
+import "sync"
+
+var once sync.Once
+var instance *sync.Map
+
+func init() {
+	once.Do(func() {
+		instance = new(sync.Map)
+	})
+}
+
+func Load(key string) (interface{}, bool) {
+	return instance.Load(key)
+}
+
+func Store(key string, value interface{}) {
+	instance.Store(key, value)
+}
+
+func LoadOrStore(key string, value interface{}) (interface{}, bool) {
+	return instance.LoadOrStore(key, value)
+}
+
+func Range(f func(key, value interface{}) bool) {
+	instance.Range(f)
+}
+
+func Delete(key interface{}) {
+	instance.Delete(key)
+}
