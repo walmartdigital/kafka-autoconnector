bfb0a086237fcb0aea017e002f582fe9961d7e8a
chinniehendrix <chinniehendrix@gmail.com>
2020-06-01 18:57:50 -0400

    feat(build.sh): install supporting K8s resources outside of skaffold to prevent them from being teared down every time the code changes

--- build.sh
+++ build.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 set -e
 
+# It is best to start Minikube with these settings -> minikube start --memory 12Gi --cpus=4
+# Requires Helm 3
+# Need to manually add Elastic repo -> helm repo add elastic https://helm.elastic.co
+
+chart=$(helm list -o json | jq -r '[.[].name] | index("elasticsearch")')
+if [ "$chart" = "null" ]
+then
+    helm install elasticsearch elastic/elasticsearch \
+        --set replicas=2 \
+        --set antiAffinity="soft" \
+        --set esJavaOpts="-Xmx128m -Xms128m" \
+        --set resources.requests.cpu="100m" \
+        --set resources.requests.memory="512M" \
+        --set resources.limits.cpu="1000m" \
+        --set resources.limits.memory="512M" \
+        --set "volumeClaimTemplate.accessModes[0]"="ReadWriteOnce" \
+        --set volumeClaimTemplate.storageClassName="standard" \
+        --set volumeClaimTemplate.resources.requests.storage="200M"
+else
+    echo "Elasticsearch is already installed, do nothing"
+fi
+kubectl patch svc elasticsearch-master -p '{"spec": {"type": "NodePort"}}'
+
+kubectl apply -f ./deploy/kafka-connect/zookeeper.yaml
+kubectl apply -f ./deploy/kafka-connect/kafka.yaml
+kubectl apply -f ./deploy/kafka-connect/kafka-connect.yaml
+
 eval $(minikube docker-env)
 temp=$(uuidgen | shasum | grep -oh '^\S*')
 
